0b48ea00db8ca634554987dd5f44d9b2b442cf29
Namazed <actek17@gmail.com>
2016-08-16 15:42:43 +0700

    Added second activity and fragment with RecyclerView. Created test list crimes in CrimeLab (Singleton)

---
+++ app/src/main/java/com/namazed/officecrime/SingleFragmentActivity.java
@@ -0,0 +1,29 @@
+package com.namazed.officecrime;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    private static final int LAYOUT = R.layout.activity_fragment;
+    private static final int CONTAINER_FRAGMENT = R.id.fragment_container;
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(LAYOUT);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(CONTAINER_FRAGMENT);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(CONTAINER_FRAGMENT, fragment).commit();
+        }
+    }
+}
